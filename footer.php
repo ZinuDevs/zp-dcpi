8f0007f5911eb992b99f68acaa775cf4f1f80a8e
Qev254 <kelvinkkaranja@gmail.com>
2024-05-26 02:15:34 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+</div> <!-- #content -->
+<footer>
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> DCPI. All rights reserved.</p>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
